987d3a20e252fb348f83d1272d38d0d195b490cd
anuosu <anuosunnuyi14@gmail.com>
2018-06-26 14:22:56 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
-
 class App < Sinatra::Base
+  
+  get '/' do
+    "My name is _"
+  end
+  
+  get '/'
+    "My hometown is _"
+  end
+  
+  get '/'
+    "My favorite-song is _"
+  end
 end
